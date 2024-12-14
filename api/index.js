4940b2c6ae2515607086248ad7e529a7f3a814ff
madn6 <msoffl0404@gmail.com>
2024-12-14 20:38:12 +0530

    server, database connection , schema and model created

---
+++ api/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const app = express();
+
+
+dotenv.config();
+
+
+async function connnectToDatabase() {
+	try {
+		const mongoURI = process.env.MONGO_URI;
+		await mongoose.connect(mongoURI);
+		console.log('connected to mongodb');
+	} catch (err) {
+		console.error('failed to connect to mongo db', err);
+		process.exit(1);
+	}
+}
+
+async function startServer() {
+	await connnectToDatabase();
+	const port = process.env.PORT || 3000;
+	app.listen(port, () => {
+		console.log('server is listenning on 3000');
+	});
+}
+
+startServer();
