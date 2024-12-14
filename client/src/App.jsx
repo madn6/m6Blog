6284539d52192475bd1850c51908dd0f4782d46e
madn6 <msoffl0404@gmail.com>
2024-12-14 14:44:51 +0530

    routing part complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home,About,Dashboard,Projects,SignIn,SignUp } from './pages/index.js';
+
 export default function App() {
   return (
-    <h1 className="">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+        <Route path='/projects' element={<Projects/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
