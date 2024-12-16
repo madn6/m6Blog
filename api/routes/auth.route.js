ab11a78f4ddfefb1b87139b6bafd065decad450b
madn6 <msoffl0404@gmail.com>
2024-12-16 12:49:03 +0530

    sign in api route created

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
-import { signUp } from '../controllers/auth.controller.js';
+import { signIn, signUp } from '../controllers/auth.controller.js';
 import express from 'express';
 
 const router = express.Router();
 
 router.post('/signup', signUp);
+router.post('/signin', signIn);
 
 export default router;
