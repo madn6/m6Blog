c02d056451d32a7371fb022957d30e0eb7bfdce2
madn6 <msoffl0404@gmail.com>
2024-12-18 22:31:10 +0530

    sidebar added

--- client/src/pages/Dashborad.jsx
+++ client/src/pages/Dashborad.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { DashProfile, DashSidebar } from '../components';
 
 export default function Dashborad() {
-  return (
-    <div>Dashborad</div>
-  )
+	const location = useLocation();
+	const [tab, setTab] = useState('');
+
+	useEffect(() => {
+		const urlParams = new URLSearchParams(location.search);
+		const tabFromUrl = urlParams.get('tab');
+		if (tabFromUrl) {
+			setTab(tabFromUrl);
+		}
+	}, [location.search]);
+	return (
+		<div className='min-h-screen flex flex-col md:flex-row'>
+			{/* sidebar */}
+			<div className="md:w-56">
+				<DashSidebar />
+			</div>
+			{/* profile */}
+			<div className="">{tab === 'profile' && <DashProfile />}</div>
+		</div>
+	);
 }
