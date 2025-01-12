f6887c3a0670d13abf0012f1d9daa33328235178
madn6 <msoffl0404@gmail.com>
2025-01-12 09:46:13 +0530

    another static file tweak

--- api/index.js
+++ api/index.js
@@ -58,8 +58,11 @@ app.use('/api/post', postRoutes);
 app.use('/api/comment', CommentRoutes);
 
 app.use(express.static(path.join(_dirname, '/client/dist')));
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(_dirname, 'client', 'dist', 'index.html'));
+});
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
 	res.status(200).json({
 		success: true,
 		message: 'Backend is running successfully',
