97b2a508c90ebc295e7b8078a93caf71d95c4eed
madn6 <msoffl0404@gmail.com>
2025-01-13 15:16:36 +0530

    tweak

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 // CORS settings
 const corsOptions = {
 	origin: (origin, callback) => {
-		const allowedOrigins = ['https://m6blog.onrender.com', 'http://localhost:5173'];
+		const allowedOrigins = ['https://m6blog.onrender.com', 'http://localhost:5173',"http://192.168.228.15"];
 
 		// Allow requests from the specified origins or any other domain (with caution)
 		if (!origin || allowedOrigins.includes(origin)) {
